49c77f190112a18a1d6b409f8e4d1461d8840b69
gaotongfei <gaotongfei7@163.com>
2015-10-19 22:01:14 +0800

    update

--- setup.py
+++ setup.py
@@ -10,12 +10,13 @@ setup(
     author_email="gaotongfei1995@gmail.com",
     url="https://github.com/gaotongfei/parchment",
     install_requires=[
-        "click",
-        "Jinja2",
-        "MarkupSafe",
-        "mistune",
-        "PyYAML",
-        "wheel"
+        "click==5.1",
+        "Cython==0.23.3"
+        "Jinja2==2.8",
+        "MarkupSafe==0.23",
+        "mistune==0.7.1",
+        "PyYAML==3.11",
+        "wheel==0.24.0"
     ],
     entry_points='''
         [console_scripts]
